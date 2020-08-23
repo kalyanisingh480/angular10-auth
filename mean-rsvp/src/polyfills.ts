ac1954ec700b206468530c67fea525b7ff111db8
Kalyani Singh <kalyanisingh480@gmail.com>
2020-08-23 19:35:04 -0400

    create the structure of app and add components

--- mean-rsvp/src/polyfills.ts
+++ mean-rsvp/src/polyfills.ts
@@ -58,6 +58,8 @@
 import 'zone.js/dist/zone';  // Included with Angular CLI.
 
 
+(window as any).global = window;
+
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
---
+++ mean-rsvp/src/server/config.js
@@ -0,0 +1,7 @@
+// server/config.js
+module.exports = {
+    AUTH0_DOMAIN: 'dev-pl3sqw3m.us.auth0.com', // e.g., kmaida.auth0.com
+    AUTH0_API_AUDIENCE: 'http://localhost:8083/api/', // e.g., 'http://localhost:8083/api/'
+    MONGO_URI: 'mongodb://kalrsvp:kalrsvp1@ds027415.mlab.com:27415/mean'
+    
+  };
