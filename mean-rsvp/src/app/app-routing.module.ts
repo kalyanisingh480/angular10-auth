61669168d2e91c2ebbb7187b006781620e8e918b
Kalyani Singh <kalyanisingh480@gmail.com>
2020-08-29 15:59:41 -0400

    complete part 2

--- mean-rsvp/src/app/app-routing.module.ts
+++ mean-rsvp/src/app/app-routing.module.ts
@@ -1,17 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
 
 const routes: Routes = [
-
-{
-  path : '',
-  component : HomeComponent,
-              HeaderComponent,
-              FooterComponent
-}
+  {
+    path : '',
+    component : HomeComponent
+  }
 ];
 
 @NgModule({
